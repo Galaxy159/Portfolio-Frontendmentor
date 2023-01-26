a6768cf2da082932e3765e473de4310960df4161
Galaxy159 <braun.asaf@gmail.com>
2023-01-26 21:09:42 +0200

    file and sections setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
-import "./App.css";
+import Header from "./components/Header";
+import Hero from "./components/Hero";
+import Features from "./components/Features";
+import Projects from "./components/Projects";
+import Contact from "./components/Contact";
+import "./style/App.css";
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <>
+      <main className="container">
+        <Header />
+        <Hero />
+        <Features />
+        <Projects />
+        <Contact />
+      </main>
+    </>
+  );
 }
 
 export default App;
