4a3e92e982a484d98bf7f8d2d401f342f82a8b87
Galaxy159 <braun.asaf@gmail.com>
2023-03-19 11:19:28 +0200

    form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,37 +2,40 @@ export default function Contact() {
   return (
     <>
       <section className="contact">
-        <div className="contact__content">
-          <h3 className="contact__content--headline">Contact</h3>
-          <p className="contact__content--text">
-            I would love to hear about your project and how I could help. Please
-            fill in the form, and I’ll get back to you as soon as possible.
-          </p>
+        <div className="contact__container">
+          <div className="contact__content">
+            <h3 className="contact__content--headline">Contact</h3>
+            <p className="contact__content--text">
+              I would love to hear about your project and how I could help.
+              Please fill in the form, and I’ll get back to you as soon as
+              possible.
+            </p>
+          </div>
+          <form action="" className="contact__form">
+            <div className="contact__form--input">
+              <input
+                className="contact__form--input-name"
+                type="text"
+                placeholder="NAME"
+              />
+            </div>
+            <div className="contact__form--input">
+              <input
+                className="contact__form--input-email"
+                type="email"
+                placeholder="EMAIL"
+              />
+            </div>
+            <div className="contact__form--input">
+              <input
+                className="contact__form--input-message"
+                type="text"
+                placeholder="MESSAGE"
+              />
+            </div>
+            <button className="contact__form--btn">SEND MESSAGE</button>
+          </form>
         </div>
-        <form action="" className="contact__form">
-          <div className="contact__form--input">
-            <input
-              className="contact__form--input-name"
-              type="text"
-              placeholder="NAME"
-            />
-          </div>
-          <div className="contact__form--input">
-            <input
-              className="contact__form--input-email"
-              type="email"
-              placeholder="EMAIL"
-            />
-          </div>
-          <div className="contact__form--input">
-            <input
-              className="contact__form--input-message"
-              type="text"
-              placeholder="MESSAGE"
-            />
-          </div>
-          <button className="contact__form--btn">SEND MESSAGE</button>
-        </form>
       </section>
     </>
   );
